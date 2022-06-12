89c167aeca3c86354620d888aa78f63eb147d147
Mohit Chhajer <56119056+mohit1029@users.noreply.github.com>
2022-06-12 17:45:35 +0530

    Update mongo.js

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
  require('dotenv').config();
 
 // Update below to match your own MongoDB connection string.
-const MONGO_URL = 'mongodb+srv://nasa-api:gUoBHaanYsrnSF2Q@nasacluster.zjmtv.mongodb.net/nasa?retryWrites=true&w=majority';
+const MONGO_URL = '';
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
